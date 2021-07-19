de75d59895150be4d2dea3aab2f235c6ea3af8f3
felip5543 <felipe77990@gmail.com>
2021-07-19 20:14:34 -0300

    initial commit

---
+++ config.js
@@ -0,0 +1,226 @@
+const ownerNumber = ["5521981218655@s.whatsapp.net"] 
+// mude somente o numero e não tire o @s.whatsapp.net
+
+const OriginalOwner = '5521981218655' 
+// mude para seu numero
+
+var gimagenotregister = false
+// quando não há comando registrado o bot procura no google image
+
+var sgooglenotregister = false
+// quando não há comando registrado o bot procura uma pesquisa no google
+
+const antinewchatmsg = 'O anti chat novo está ativado e você não é meu proprietário, logo será bloqueado'
+//mensagem do antinewchat
+
+const urlaudiomenu = 'https://b.top4top.io/m_19923zx481.mp3'
+// link direto da url do audio
+
+const antipvmsg = '*🚫 PV BLOQUEADO 🚫*'
+//mensagem do antipv
+
+const limitqnt = 9999
+
+const delayantispamcmd = 3
+//tempo de delay do antispamcmd (medido por segundos)
+
+const msgwelcomeimg = (numero, groupname) => {
+    return `BEM-VINDO KRL ${numero}\n\nEscreve ${prefix}MENU PRA TU VER MEUS COMANDOS`
+}
+//Texto de boas vindas na imagem quando welcome esta ativado
+//Obs: não coloque o texto longo para a imagem não sumir
+
+const byemsgimg = (numero, groupname) => {
+    return `ADEUS ${numero}`
+}
+//Texto de despedidas na imagem quando welcome esta ativado
+//Obs: não coloque o texto longo para a imagem não sumir
+
+const menumsgimg = (numero, groupname, battery) => {
+    return `⏤͟͟͞͞ঔৣ͜͡𝖆𝚟𝖆𝖑𝖆𝖓𝖈𝖍𝖊᭄ꦿ BOT com 434 COMANDOS, BATERIA: ${battery}`
+}
+//Texto no menu da imagem quando welcome esta ativado
+//Obs: não coloque o texto longo para a imagem não sumir
+
+const titlemenumsgimg = 'BEM-VINDO'
+//Título no menu da imagem quando welcome esta ativado
+//Obs: não coloque o texto longo para a imagem não sumir
+
+const backgroundmenuimg = `https://picfiles.alphacoders.com/218/218496.jpg`
+//Link direto da imagem de fundo TAMANHO RECOMENDADO: 1280x720
+//Obs: não coloque o texto longo para a imagem não sumir
+
+const backgroundwelcomeimg = `https://picfiles.alphacoders.com/218/218496.jpg`
+//Link direto da imagem de fundo da boas-vindas TAMANHO RECOMENDADO: 1280x720
+//Obs: não coloque o texto longo para a imagem não sumir
+
+const backgroundbyeimg = `http://obviousmag.org/pensando_nessa_gente_da_vida/adeus.jpg`
+//Link direto da imagem de fundo das despedidas TAMANHO RECOMENDADO: 1280x720
+//Obs: não coloque o texto longo para a imagem não sumir
+
+const apikeyimgbb = 'c9cf132756510ad171587fb5a5a0389f'
+// sua key do imgbb para ativar o antiporn e mais alguns comando que necessita de baixar imagem
+
+const cr = '☄️⏤͟͟͞͞ঔৣ͜͡𝖆𝚟𝖆𝖑𝖆𝖓𝖈𝖍𝖊᭄ꦿ☄️\n' 
+// texto do simbolo de verificado
+
+const prefix = '!' 
+// prefixo
+
+const blockedmsg = '*Seu número esta bloqueado, ou seja não ouvindo você TROXA KKK*'
+// mensagem quando alguem bloqueado solicita um comando
+
+const blockedcmdmsg = '*🚫Este comando esta bloqueado contate o propitetário do bot para saber o pq🚫*'
+// mensagem quando alguem solicita um comando bloqueado 
+
+// MENSAGENS DE BAN
+
+const banmsgtype = '*Corra membro comum, jaja o seu ban chega*'
+// mensagem de ban no anti tipos de mensagem
+
+const adminmsgtype = '*a msg é proibida, mas vc é adm entao está de boa*'
+// mensagem de quando adm manda tipos de mensagens proibidas
+
+const banmsgporn = 'Hmmm pornozinho com antiporn ativado, ja sabe né...'
+// mensagem de ban no antiporn
+
+const adminmsgporn = '*Tu é adm ent n vou te banir por mandar porn*'
+// mensagem de quando adm manda porn com antiporn ativado
+
+const banmsglink = 'Corra Membro comum, O ban está próximo...'
+// mensagem de ban no antilink
+
+const adminmsglink = 'Isso é um link amigo... ah é tu é adm ent pode kkkk 🙃'
+// mensagem de quando adm manda link
+
+const adminmsgpalavra = 'isso esta na lista das palavras proibidas pelo bot'
+// mensagem de quando adm manda uma palavra proibida pelo bot
+
+const banmsgpalavra = 'Palavra proibida? Já sabe né'
+// mensagem de ban quando manda uma palavra proibida pelo bot
+
+const qnttravahardroleta = 20
+// quantidade de travas que mandam no roleta russa hard
+// Obs: O valor é multiplicado por 12 ent tenha cuidado
+
+// MENSAGENS DE BOM DIA BOA TARDE E BOA NOITE
+
+const linkimgday = 'http://4.bp.blogspot.com/-_uVCihPbmuo/VSHSwtU76dI/AAAAAAAAFcM/aZrjTNC3AX4/s1600/IMG-20150405-WA0004.jpg'
+const textmsgday = 'BOM DIA NESSA DISGRAÇAAA'
+// link e mensagem de bom dia
+
+const linkimgeve = 'https://i.ibb.co/bLXgcBW/lp-tarde-anime-by-hitsukinyan-db7fpqr-fullview.jpg'
+const textmsgeve = `Kon'nichiwa Onii-chan, aceite esse café ☕`
+// link e mensagem de boa tarde
+
+const linkimgnig = 'https://i.ytimg.com/vi/CF9nWGfuQjQ/hqdefault.jpg'
+const textmsgnig = 'VAI DORMIR, VOÇE É UM CORNO E NAO UM MORCEGO'
+// link e mensagem de boa noite 
+
+// MENSAGENS DE ERRO
+
+const msgerr = 'OCORREU UM ERRO, TENTE AI BROW :/'
+// mensagem de erro
+
+const notregister = `*Comando não registrado, escreva ${prefix}menu para ver a lista de comandos*`
+
+// MENSAGENS DE AUTOREPLY
+
+const botlindo = 'VALEU PUTAO'
+const linkbotlindo = 'https://photos1.iorbix.com/00/00/00/00/02/72/43/64/C--6tZwaASQH-b.jpg'
+const botfeio = 'Tu parece um sirgueijo, um caranguejo amassado 😡'
+const cadebot = 'Me chamou pcr?'
+const botfdp = 'Te foder rapaz, te deitar na porrada quando tiver dormindo'
+const botgostoso = 'somos 2 bb'
+const botfofo = 'muio obrigado bb'
+const botbaianor = 'teu pai aquele chifrudo'
+const botcorno = 'Vai te foder, jogador de ff, comprador de pack do pézinho'
+const botputa = 'sua mãe 😡'
+const botgay = 'Falou o manja rola 😡'
+const botviado = 'meu pai atras da sua mae de quatro'
+const numbotfeio = 'steu cu é a roda meu pau é o freio'
+// KEYS
+
+// MENSAGENS DE EXCLUSIVIDADE
+let mess = {
+    wait: '⌛ DE UMA AGUARDADA AI BROW...',
+    success: '✔️ Sucesso! ✔️',
+    error: {
+        stick: '❌ Falha, ocorreu um erro ao converter a imagem em um adesivo ❌',
+        Iv: '❌ Link inválido ❌'
+    },
+    only: {
+        group: '❌ Este comando só pode ser usado em grupos! ❌',
+        ownerG: '❌ Este comando só pode ser usado pelo grupo proprietário! ❌',
+        ownerB: '❌ Este comando só pode ser usado pelo número proprietário! ❌',
+        admin: '❌ SILÊNCIO MEMBRO COMUM VC N TEM MORAL PRA USAR ESSE COMANDO ❌',
+        Badmin: '❌ Este comando só pode ser usado quando o bot se torna administrador! ❌'
+    }
+}
+
+// CONTATO DO CRIADOR
+
+const vcard = 'BEGIN:VCARD\n' 
++ 'VERSION:3.0\n' 
++ 'FN:FP^~^\n' 
++ 'ORG:FP;\n' 
++ 'TEL;type=CELL;type=VOICE;waid=5521981218655:+55 21 98121-8655\n' 
++ 'END:VCARD'
+//vcard do propietário, mude os numeros de acordo com o formato que
+//ele corresponde
+
+
+// Nem ouse mexer aqui se n quiser que de erro
+exports.limitqnt = limitqnt
+exports.urlaudiomenu = urlaudiomenu
+exports.delayantispamcmd = delayantispamcmd
+exports.antinewchatmsg = antinewchatmsg
+exports.antipvmsg = antipvmsg
+exports.backgroundbyeimg = backgroundbyeimg
+exports.backgroundwelcomeimg = backgroundwelcomeimg
+exports.backgroundmenuimg = backgroundmenuimg
+exports.titlemenumsgimg = titlemenumsgimg
+exports.menumsgimg = menumsgimg
+exports.gimagenotregister = gimagenotregister
+exports.sgooglenotregister = sgooglenotregister
+exports.byemsgimg = byemsgimg
+exports.msgwelcomeimg = msgwelcomeimg
+exports.notregister = notregister
+exports.qnttravahardroleta = qnttravahardroleta
+exports.blockedcmdmsg = blockedcmdmsg
+exports.blockedmsg = blockedmsg
+exports.banmsgpalavra = banmsgpalavra
+exports.adminmsgpalavra = adminmsgpalavra
+exports.apikeyimgbb = apikeyimgbb
+exports.linkbotlindo = linkbotlindo
+exports.botlindo = botlindo
+exports.botfeio = botfeio
+exports.cadebot = cadebot
+exports.botfdp = botfdp
+exports.botgostoso = botgostoso
+exports.botfofo = botfofo
+exports.botbaianor = botbaianor
+exports.botcorno = botcorno
+exports.botputa = botputa
+exports.botgay = botgay
+exports.botviado = botviado
+exports.numbotfeio = numbotfeio
+exports.linkimgday = linkimgday
+exports.textmsgday = textmsgday
+exports.textmsgnig = textmsgnig
+exports.linkimgnig = linkimgnig
+exports.textmsgeve = textmsgeve
+exports.linkimgeve = linkimgeve
+exports.adminmsgtype = adminmsgtype
+exports.adminmsgporn = adminmsgporn
+exports.adminmsglink = adminmsglink
+exports.banmsglink = banmsglink
+exports.banmsgporn = banmsgporn
+exports.banmsgtype = banmsgtype
+exports.msgerr = msgerr
+exports.mess = mess
+exports.prefix = prefix
+exports.cr = cr
+exports.vcard = vcard
+exports.OriginalOwner = OriginalOwner
+exports.ownerNumber = ownerNumber
